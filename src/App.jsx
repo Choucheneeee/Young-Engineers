3377dda48c896469635c6cac535dd8d7eb92e977
Dheker Kraiem <dheker.krm@gmail.com>
2024-12-15 01:06:40 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Login from "./components/Auth/LoginForm";
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+  <div>
+    <Login />
+  </div>
+  );
 }
 
 export default App;
